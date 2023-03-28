8d0c38fa8a80036692f3e5e48f32af411b6a670b
Mike Will <myQwil@gmail.com>
2023-03-28 10:43:35 -0400

    add an option to change the rate of zoom mode
    While `--zoom-steps` is the number of button or key presses between
    powers of 2, `--zoom-rate` represents the number of pixels between
    powers of 2 that the user clicks and drags between, so if you added
    the arg `--zoom-rate=1024`, it would result in a very slow zoom mode.
    The default is 128 pixels.

--- src/options.h
+++ src/options.h
@@ -128,6 +128,7 @@ struct __fehoptions {
 	int offset_y;
 	int default_zoom;
 	int zoom_mode;
+	double zoom_rate;
 	double step_rate;
 	unsigned char adjust_reload;
 	int xinerama_index;
@@ -217,6 +218,7 @@ OPTION_bg_scale,
 OPTION_bg_fill,
 OPTION_bg_max,
 OPTION_zoom,
+OPTION_zoom_rate,
 OPTION_zoom_step,
 OPTION_zoom_in_rate,
 OPTION_zoom_out_rate,
